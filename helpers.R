d9f23eaf61dacdaea119d1f43f27cbb545235d6c
Koen Leuveld <koen.leuveld@wur.nl>
2024-10-10 13:52:02 +0200

    rename and add winsorize

--- helpers.R
+++ helpers.R
@@ -4,8 +4,11 @@
 create_codebook <- function(.df,stats = list(mean=mean,sd=sd,min=min,max=max,
                                              prop_miss=prop_miss)) {
   
-  
-  # import functions
+  # takes a dataframe, and outputs a dataframe with variable names, labels
+  # and some summ stats
+  # useful to get a first sense of a data set.
+
+  # import packages
   box::use(r/core[...])  
   box::use(dplyr[`%>%`, coalesce, mutate, bind_cols ])
   box::use(tibble[tibble, as_tibble_row])
@@ -37,14 +40,36 @@ create_codebook <- function(.df,stats = list(mean=mean,sd=sd,min=min,max=max,
 
 
 
-stacked_bar_plot <- function(df, by, fill, textsize = 20, 
-                             legendposition = "bottom",legendrows = 2,
+winsorize <- function(x, lower = 0.05, upper = 0.95){
+  
+  # remove outliers
+  # I didn't want to include a whole library just for this.
+  
+  lower_bound <- quantile(x, lower)  # 5th percentile
+  upper_bound <- quantile(x, upper)  # 95th percentile
+  
+  #Winsorize: Replace values outside the bounds with the respective bounds
+  case_when(
+    x < lower_bound ~ lower_bound,
+    x > upper_bound ~ upper_bound,
+    TRUE ~ x
+  )
+}
+
+
+
+stacked_bar_plot <- function(df, fill, by, textsize = 20, 
+                             legendposition = "bottom", legendrows = 2,
                              labs = NULL) {
-  
-  # note that you still need to load ggthemewur in the calling script!
-  # if not, the fonts won't work.
-  
-  # import functions
+
+  # takes a dataframe and two categorical variables (fill and by)
+  # outputs a plot stacked bar chart, where:
+  # the by variables will be on the y-axis
+  # the x-axis will be counts of the fill variable
+  # the plot will include counts of the fill variables
+  # to include counts of the by variable, use count_label
+
+  # import packages
   box::use(r/core[...])  
   box::use(dplyr[...])
   box::use(ggplot2[...])
@@ -76,7 +101,13 @@ stacked_bar_plot <- function(df, by, fill, textsize = 20,
 
 
 count_label <- function(vector) {
-  
+
+#  takes a factor or string vector
+# outputs same vector, as a factor, with counts appended after a line break
+# "category 1" -> "category1\nn=1"  
+# useful for plotting
+
+  # import packages
   box::use(dplyr[...])
   box::use(forcats[fct_recode])
   
@@ -94,5 +125,3 @@ count_label <- function(vector) {
                pull(value, name = newlabel))
   
 }
-
-
