2a8ff7a2a6516ef5157476b0156af8d0c7948d89
Koen Leuveld-Postma <koen.leuveld@wur.nl>
2025-04-10 15:45:31 +0200

    add odk_helpers.R, and update documentation

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # Introduction
 
 [This repo](https://github.com/kleuveld/box_modules) contains two R scripts with helper functions, utils.R and plots.R and (see function index).
-These scripts can be called using the `[box](https://github.com/klmr/box)` package, 
+These scripts can be called using the [`box`](https://github.com/klmr/box) package, 
 in effect working just like a 
 
 # How to use
 
 - [Read how to use the box package](https://klmr.me/box/articles/box.html).
 - Clone this repo, or copy the files anywhere on your device, for example: `c:\path\to\box_modules`
-- add `options('box.path' = "C:/path/to/")` 
-(so the parent folder of the path where the scripts are) to
- `[~/.Rprofile](https://support.posit.co/hc/en-us/articles/360047157094-Managing-R-with-Rprofile-Renviron-Rprofile-site-Renviron-site-rsession-conf-and-repos-conf)`
+- add `options('box.path' = "C:/path/to/")`  to
+ [`~/.Rprofile`](https://support.posit.co/hc/en-us/articles/360047157094-Managing-R-with-Rprofile-Renviron-Rprofile-site-Renviron-site-rsession-conf-and-repos-conf) 
+ (NB: that's the parent folder of the folder where your scripts are in!)
 - In the script where you want to use a function use `box::use(box_modules/utils)` so the functions are loaded.
 - Call the function using the form `script$function()`, 
 so for example to winsorize a variable use `mutate(var = utils$winsorize(var)`
-
+- To find the modules using [`here`](https://here.r-lib.org/),
+make sure the modules are within your project folder and
+add `options(box.path = here::here(""))` to the 
+`.Rprofile` of your project. Use `box::use(relativepath/to/box_modules/utils)`
+to load a module, where the relative path starts at the project root.
 
 # Function index
+
+For now you'll need to have a look at the actual function to see how to use them.
+Better documentation may be coming.
 
 - utils.R: utility function to manage data. 
    - create_codebook: creates a "codebook"-dataframe containing variable labels and summ stats of data frame.
@@ -24,10 +31,28 @@ so for example to winsorize a variable use `mutate(var = utils$winsorize(var)`
    - count_label: adds counts to the labels of factors, useful for some plots
    - truncate_sample: truncates a vector by resampling out-of-bounds observations
    from within-bounds observations. Useful for generating fake data.
-   - isid: checks if a vector has duplicates, and stops executing scripts if
-   it does.
+   - isid: checks if a data frame is uniquely identified by the variables 
+   supplied, and stops executing scripts if it is not.
    Useful after generating ID codes to ensure they are valid.
 - plots.R: plots:
   - stacked_bar_plot: a stacked bar chart.
   - stacked_bar_plot_wur: the same, but in WUR housestyle. Requires the `[ggthemewur](https://git.wur.nl/wmrkdown/ggthemewur)` package.
-
+- odk_helpers.R
+  - load_data(): get data exported by `ruODK::submission_export()`, load and label it. Arguments:
+    - data_loc: the location where `ruODK` put your data.
+    - form_id: the form_id
+    - form_schema: the schema of the form. Defaults to `ruODK::form_schema()`
+    - xlsform: the xlsform that contains your form definition.
+  Returns a list of dataframes.
+  - reshape_subtable: takes a dataframe, and pivots it wider to the shape of its parent.
+  - merge_subtables: create one wide dataset of a table and all its subtables.
+Arguments:
+    - .data: list as outputed by `load_data()`
+    - table: name of the table to merge subtables to. 
+Defaults to "main", to create one wide data frame of the entire data set.
+    - form_schema: the schema of the form. Defaults to `ruODK::form_schema()`
+  - get_group_duration: get durations of groups from the audit log.  Arguments:
+    - audit_log: path to audit log csv
+    - group: group name to get duration of 
+    - winsorize: set to TRUE to winsorize durations. Defaults to FALSE. 
+  Requires the utlis.R file to be present in the same folder.
---
+++ odk_helpers.R
@@ -0,0 +1,218 @@
+
+load_data <- function(
+    data_loc,form_id,form_schema = ruODK::form_schema(),xlsform) {
+
+  box::use(r/core[...])  
+  box::use(dplyr[...])
+  box::use(tibble[...])
+  box::use(purrr[...])
+  box::use(readxl[...])
+  box::use(tidyr[...])
+  box::use(stringr[...])
+  box::use(readr[...])
+  box::use(here[...])
+  box::use(janitor[...])
+
+
+  # Fetch form schema
+  #form_schema <- ruODK::form_schema()
+
+  # get all the group names from the schema, so we can remove them from varnames
+  group_names <- 
+   form_schema %>%
+   filter(type == "structure") %>%
+   pull(name)
+
+  clean_names <- function(df, group_names) {
+
+    #remove group names using a regex containing all the group names
+    pattern <- paste(paste0(group_names,"_?"), collapse = "|")
+    colnames(df) <- gsub(pattern, "", colnames(df))
+
+    # make sure colnames are unique
+    colnames(df) <- make_clean_names(colnames(df), unique_sep = "_")
+    
+    return(df)
+  }
+
+  # label definition
+  xls_form_choices <- 
+    read_excel(xlsform,
+               sheet = "choices") %>% 
+    filter(!is.na(list_name)) 
+
+  labels <-  
+   xls_form_choices %>%
+    distinct(list_name) %>%
+    pull(list_name, name = list_name) %>% 
+    map( ~ xls_form_choices %>%
+           filter(list_name == .x ) %>%
+           with(list(name = name, label = `label::English (en)`)))
+
+
+
+  # variable mapping to labels
+  xls_form_survey <- 
+    read_excel(xlsform,
+               sheet = "survey") 
+
+
+  # a tibble of variable names and the labelset to apply to them
+  select_one <-
+    xls_form_survey %>%
+    filter(str_detect(type,"^select_one")) %>%
+    separate(type, into = c("type", "list"), sep = " ", extra = "merge") %>%
+    filter(!str_detect(list,"^\\$\\{.+\\}$")) %>%
+    filter(!str_detect(list,"\\.csv$")) %>%
+    select(name,list)
+
+
+  # function to apply labels to select_one questions
+  label_select_one <- function(df,labels,vars){
+
+    for (var in names(df)){
+      if (var %in% vars$name){
+        labelset <-
+          select_one %>%
+          filter(name == var) %>%
+          pull(list) 
+
+        df[[var]] <-  factor(df[[var]],
+                             levels = labels[[labelset]]$name,
+                             labels = labels[[labelset]]$label)
+
+      }
+
+    }
+    df
+  }
+
+  table_names <-
+   c("main",
+      form_schema %>%
+      filter(type == "repeat") %>%
+      pull(name))
+
+
+  file_names <- 
+      paste0(c(form_id,
+               paste(form_id,
+                     table_names[2:length(table_names)],sep ="-")),
+            ".csv")
+
+
+  all_data <-
+  file_names %>%
+    set_names(table_names) %>%
+    map(~ read_csv(here(data_loc,.x))) %>%
+    map( ~ clean_names(.x,group_names)) %>%
+    map(~ label_select_one(.x,labels = labels, vars = select_one))
+
+
+
+
+    return(all_data)
+}
+
+
+# reshape the data to one wide data set using two function
+
+# one to reshape a subtable to the shape of its parent
+
+reshape_subtable <- function(df){
+  box::use(r/core[...])  
+  box::use(dplyr[...])
+  box::use(tidyr[...])
+  box::use(stringr[...])
+
+
+  # you don't need to reshape something that doesn't have a parent...
+  if ("parent_key" %in% names(df)) {
+    output <- 
+      df %>%
+        mutate(key = str_extract(key, "\\[([0-9]+)\\]$", group = 1)) %>%
+        pivot_wider(id_cols = parent_key, names_from = key, values_from = -parent_key) %>%
+        rename(key = parent_key)
+
+  } else {
+    output <- df
+  }
+  
+  output
+}  
+
+# and one two merge all subtables, recursively
+
+merge_subtables <- function(.data,table = "main", form_schema = ruODK::form_schema()) {
+
+  box::use(r/core[...])  
+  box::use(dplyr[...])
+  box::use(tibble[...])
+  box::use(purrr[...])
+  box::use(tidyr[...])
+  box::use(stringr[...])
+
+  # get a list of subtables
+  get_parent <- function(path,name) {
+    pattern <- paste0("(?<=/)(",paste(name, collapse = "|"),")(?=/)")
+    map_chr(path,~{
+            matches <-  str_match_all(.x, pattern)[[1]][,1]  %>% tail(1)
+            if (length(matches) > 0) tail(matches, 1) else "main"
+            }) 
+  }
+
+  subtables <-
+      form_schema %>% 
+      filter(type == "repeat") %>%
+      mutate(parent = get_parent(path,name)) %>%
+      select(name,parent) %>%
+      filter(parent == table) %>%
+      pull(name, name = name)
+
+
+  # if no subtables, return reshaped subtable
+  if (length(subtables) == 0) {
+     output <-
+       .data[[table]] %>%
+       reshape_subtable()
+     
+  } else {
+    # if there are subtables, run this function on all of them
+    # (effectively reshaping all tables to fit their parents)
+    # and merge it all togther
+      output <-
+     left_join(.data[[table]] %>% reshape_subtable(),
+               subtables %>%
+                 map(~merge_subtables(.data,.x,form_schema)) %>%
+                 reduce(~left_join(.x,.y,by = join_by(key))),
+               by = join_by(key))
+  }
+  output
+}
+
+
+
+get_group_duration <- function(audit_log,group, winsorize = FALSE) {
+
+  box::use(r/core[...])  
+  box::use(dplyr[...])
+  box::use(stringr[str_detect, str_extract])
+
+  audit_log %>%
+    #select(`instance ID`, node, start, end) %>%
+    filter(str_detect(node, group)) %>%
+    filter(event %in% c("question","group questions")) %>%
+    mutate(duration = (end - start) / (1000 * 60)) %>% 
+    {
+      if (winsorize) {
+        box::use(./utils)
+        {.} %>% 
+          mutate(name = str_extract(node, "[^/]+$")) %>%
+          mutate(duration = utils$winsorize(duration),.by = name)
+      } else {
+        {.}
+
+      }
+    } %>%
+    summarize(group = group, duration = sum(duration,na.rm = TRUE), .by = `instance ID`)  
+}
